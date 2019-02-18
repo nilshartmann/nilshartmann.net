e87bbb61fd7c19459bf9d5b167ade88777f5da44
Nils Hartmann <nils@nilshartmann.net>
2019-02-18 21:00:23 +0100

    new folder structure

--- copy-to-server.sh
+++ copy-to-server.sh
@@ -4,4 +4,4 @@ yarn build
 
 git rev-parse HEAD >public/git.txt
 
-rsync -a --delete --stats public/ nils@nilshartmann.net:/home/nils/htdocs/nilshartmann.net
+rsync -a --delete --stats public/ nils@nilshartmann.net:/home/nils/htdocs/nilshartmann.net/gatsby
